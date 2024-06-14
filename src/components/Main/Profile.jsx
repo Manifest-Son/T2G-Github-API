3def9adc1e6c1d418822137f38af477cecc305e8
manifest_son <lennoxgithinji37@gmail.com>
2024-06-14 18:30:54 +0300

    sets up the final frontend view

--- src/components/Main/Profile.jsx
+++ src/components/Main/Profile.jsx
@@ -1,5 +1,38 @@
+import johnImg from "../../assets/john.jpg";
+import { IoLocationOutline } from "react-icons/io5";
+import { FaBuilding } from "react-icons/fa";
+import { IoIosPeople } from "react-icons/io";
+import { FaBook } from "react-icons/fa6";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import "./Main.css";
+
 const Profile = () => {
-  return <h1>Hellooooooooooooo!!!</h1>;
+  return (
+    <div className="profile-container">
+      <img src={johnImg} alt="A guys profile" className="profile-img" />
+      <h1>Name</h1>
+      <p>Username</p>
+      <p>Description</p>
+      <button className="profile-link">
+        <FaExternalLinkAlt /> View on GitHub
+      </button>
+      <p>
+        <IoLocationOutline /> Location
+      </p>
+      <p>
+        <FaBuilding /> Company
+      </p>
+      <p>
+        <FaBook /> Repositories
+      </p>
+      <p>
+        <IoIosPeople /> num following
+      </p>
+      <p>
+        <IoIosPeople /> num followers
+      </p>
+    </div>
+  );
 };
 
 export default Profile;
