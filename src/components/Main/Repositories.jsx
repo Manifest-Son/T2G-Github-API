a900e9d2583a356d2152e2169145dc7e84b99b40
manifest_son <lennoxgithinji37@gmail.com>
2024-06-19 01:56:25 +0300

    sets up the repository api section

--- src/components/Main/Repositories.jsx
+++ src/components/Main/Repositories.jsx
@@ -1,5 +1,42 @@
+import "./Main.css";
+import useStore from "../../store/store";
+import { FaCodeFork, FaStar } from "react-icons/fa6";
+
 const Repositories = () => {
-  return <h1>Hello World</h1>;
+  const loading = useStore((state) => state.loading);
+  const error = useStore((state) => state.error);
+  const repoList = useStore((state) => state.repoList);
+
+  if (loading) {
+    return <p>Loading repositories....</p>;
+  }
+  if (error) {
+    return <p className="error-message">Error: {error}</p>;
+  }
+
+  return (
+    <div className="repository-container">
+      <h1>Repositories (number)</h1>
+      <div className="repository-wrapper">
+        {repoList.map((repo) => (
+          <div className="repo" id={repo.repo_id} key={repo.repo_id}>
+            <div className="repos">
+              <h2>{repo.repo_name}</h2>
+              <p>{repo.repo_description}</p>
+            </div>
+            <div className="repos-bottom">
+              <p>
+                <FaCodeFork /> {repo.forks} forks
+              </p>
+              <p>
+                <FaStar /> {repo.stars} stars
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Repositories;
--- src/store/store.js
+++ src/store/store.js
@@ -46,6 +46,37 @@ const Store = (set) => ({
       console.error("Error fetching profile:", error);
     }
   },
+
+  repo_id: "",
+  repo_name: "",
+  repo_description: "",
+  forks: "",
+  stars: "",
+  repoList: [],
+  fetchRepo: async (username) => {
+    set({ loading: true, error: null });
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${username}/repos`,
+      );
+      const data = await response.json();
+      if (response.ok) {
+        const repos = data.map((current) => ({
+          repo_id: current.id,
+          repo_name: current.name,
+          repo_description: current.description,
+          forks: current.forks_count,
+          stars: current.stargazers_count,
+        }));
+        set({ repoList: repos, loading: false });
+      } else {
+        throw new Error(data.message);
+      }
+    } catch (error) {
+      set({ loading: false, error: error.message });
+      console.error("Error fetching repositories", error);
+    }
+  },
 });
 
 const useStore = create(Store);
