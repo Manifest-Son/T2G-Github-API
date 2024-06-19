d776d43c6d2297ee53c97684b620f1ef9c7f4753
manifest_son <lennoxgithinji37@gmail.com>
2024-06-19 07:40:25 +0300

    sets up the whole working environment

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,8 +3,24 @@ import Repositories from "./Repositories";
 import Following from "./Following";
 import Followers from "./Followers";
 import "./Main.css";
+// import { useEffect } from "react";
+// import useStore from "../../store/store";
 
 const Main = () => {
+  // const {username} = useStore()
+  // const useUseProfile = useStore((state) => state.useUserProfile);
+  // const useUserRepo = useStore((state) => state.useUserRepo);
+  // const useUserFollowers = useStore((state) => state.useUserFollowers);
+  // const useUserFollowing = useStore((state) => state.useUserFollowing);
+
+  // useEffect(() => {
+  //   if (username) {
+  //     useUseProfile(username);
+  //     useUserRepo(username);
+  //     useUserFollowers(username);
+  //     useUserFollowing(username);
+  //   }
+  // }, [username, useUseProfile, useUserRepo, useUserFollowers, useUserFollowing]);
   return (
     <div className="main-container">
       <div className="left-content">
