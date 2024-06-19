155f042e22be736c12b088d350d03ae406f832b2
manifest_son <lennoxgithinji37@gmail.com>
2024-06-19 15:18:08 +0300

    fixes the functionality

--- src/components/Main/Profile.jsx
+++ src/components/Main/Profile.jsx
@@ -4,54 +4,70 @@ import { IoIosPeople } from "react-icons/io";
 import { FaBook } from "react-icons/fa6";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import "./Main.css";
+import { useEffect } from "react";
 import useStore from "../../store/store";
 
 const Profile = () => {
   const loading = useStore((state) => state.loading);
   const error = useStore((state) => state.error);
-  const {
-    username,
-    names,
-    gitImg,
-    userUrl,
-    location,
-    company,
-    followers,
-    following,
-    userBio,
-    repos,
-  } = useStore();
+  const { username } = useStore();
+  const [profile, setProfile] = useStore();
 
-  if (loading) {
-    return <p>Loading repositories....</p>;
+  useEffect(() => {
+    const useUserProfile = async (username) => {
+      loading(true);
+      error(false);
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${username}`,
+        );
+        const data = await response.json();
+        setProfile(data);
+      } catch {
+        error(true), error(error.message);
+        console.error("Error fetching profile:", error);
+      } finally {
+        loading(false);
+      }
+    };
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useUserProfile(username);
+  });
+
+  if (loading == true) {
+    return <div className="loading-message">Loading ......</div>;
   }
-  if (error) {
-    return <p className="error-message">Error: {error}</p>;
+  if (error == true) {
+    return <div className="error-message">An error occured ......</div>;
   }
 
   return (
     <div className="profile-container">
-      <img src={gitImg} alt="A guys profile" className="profile-img" />
-      <h1>{names}</h1>
-      <p>{username}</p>
-      <p>{userBio}</p>
-      <button className={userUrl}>
+      <img
+        src={profile.avatar_url}
+        alt="A guys profile"
+        className="profile-img"
+      />
+      <h1>{profile.name}</h1>
+      <p>{profile.login}</p>
+      <p>{profile.bio}</p>
+      <button className={profile.url}>
         <FaExternalLinkAlt /> View on GitHub
       </button>
       <p>
-        <IoLocationOutline /> {location}
+        <IoLocationOutline /> {profile.location}
       </p>
       <p>
-        <FaBuilding /> {company}
+        <FaBuilding /> {profile.company}
       </p>
       <p>
-        <FaBook /> {repos}
+        <FaBook /> {profile.public_repos}
       </p>
       <p>
-        <IoIosPeople /> {following} following
+        <IoIosPeople /> {profile.following} following
       </p>
       <p>
-        <IoIosPeople /> {followers} followers
+        <IoIosPeople /> {profile.followers} followers
       </p>
     </div>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -2,50 +2,10 @@ import { create } from "zustand";
 
 const Store = (set) => ({
   username: "github",
-  names: "",
-  gitImg: "",
-  userUrl: "",
-  location: "",
-  company: "",
-  followers: "",
-  following: "",
-  userBio: "",
-  repos: "",
-
   loading: null,
   error: null,
 
   setUser: (username) => set({ username }),
-
-  useUserProfile: async (username) => {
-    set({ loading: true, error: null });
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      if (!response) {
-        const error = new error("Failed to fetch data");
-        set({ loading: false, error: error.message });
-        console.error(
-          "There has been an error in fetching the data.",
-          error.message,
-        );
-      }
-      const data = await response.json();
-      set({
-        name: data.name,
-        gitImg: data.avatar_url,
-        userUrl: data.url,
-        location: data.location,
-        company: data.company,
-        repos: data.public_repos,
-        followers: data.followers,
-        following: data.following,
-        userBio: data.bio,
-      });
-    } catch (error) {
-      set({ loading: false, error: error.message });
-      console.error("Error fetching profile:", error);
-    }
-  },
 
   repo_id: "",
   repo_name: "",
